7f5a8934a92bde2cb022ce6432b84bd52bc4c0de
sambhavchapagain <sharmasambhav2064@gmail.com>
2026-02-01 21:59:18 +0545

    Initialize app

---
+++ lib/data/network/NetworkApiSerivices.dart
@@ -0,0 +1,7 @@
+import 'package:providers_app/data/network/BaseApiServices.dart';
+import 'package:providers_app/data/response/api_response.dart';
+
+
+class NetworkApiSerivices extends BaseApiSerivice{
+
+}
