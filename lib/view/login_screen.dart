659c975514d63253e4cd21b14fc2107fe13f9772
sambhavchapagain <sharmasambhav2064@gmail.com>
2026-02-03 23:30:31 +0545

    hshgas

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:providers_app/respository/components/round_button.dart';
 import 'package:providers_app/utils/routes/routes_name.dart';
 import 'package:providers_app/utils/utils.dart';
 import 'package:providers_app/view/home_screen.dart';
@@ -21,6 +22,7 @@ FocusNode passwordFocusNode = FocusNode();
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height * 1;
     return Scaffold(appBar: AppBar(
       title: Text('Login'),
       centerTitle: true,
@@ -56,20 +58,49 @@ class _LoginScreenState extends State<LoginScreen> {
                 obscureText: _obsecurePassword.value,
               focusNode:passwordFocusNode,
               obscuringCharacter: '*',
-              decoration: const InputDecoration(
+              decoration:  InputDecoration(
                   hintText: 'P@123',
                   labelText: 'Password',
                   prefixIcon: Icon(Icons.password_sharp),
                 suffixIcon: InkWell(
+                  onTap: (){
+                    _obsecurePassword.value = !_obsecurePassword.value;
 
 
-                  child: const Icon(Icons.visibility),
+
+                  },
+
+
+                  child:  Icon(
+                      _obsecurePassword.value ? Icons.visibility_off_outlined:
+                  Icons.visibility),
                 ),
 
               ),
             );
           },
           ),
+            SizedBox(height:height*.085,
+            ),
+            RoundButton(
+              title: 'login',
+                onPress: (){
+                if(_emailController.text.isEmpty){
+                  Utils.flushBarErrorMessage('please enter a email', context);
+
+                }else if( _passwordController.text.isEmpty){
+                  Utils.flushBarErrorMessage('please enter a password', context);
+                }else if(_passwordController.text.length< 6) {
+                  Utils.flushBarErrorMessage('please enter 6 digits paswword ', context);
+
+                }
+                else{
+                  print('api hit');
+                }
+
+                },
+            ),
+
 
           ],
         ),
